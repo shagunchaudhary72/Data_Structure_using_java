175f64c66fef3e108d1b131544c198dd8c5f37b8
47259_Shagun <chshagun72@gmail.com>
2021-10-10 22:42:30 +0530

    index of first occurenece

--- Searching/FirstOccureenceBinarySearch.java
+++ Searching/FirstOccureenceBinarySearch.java
@@ -2,24 +2,41 @@ import java.util.Scanner;
 
 public class FirstOccureenceBinarySearch {
     
+    //For Sorted Arrays
+    // static int firstOccurence( int[] arr, int num ){
+    //     int result = -1;      
+    //     int low = 0;
+    //     int high = arr.length - 1;
+    //     while( low <= high ){
+    //         int middle = ( low + high )/2;
+    //         if( num < arr[middle] ){
+    //             high = middle - 1;
+    //         }else if( num > arr[middle] ){
+    //             low = middle + 1;
+    //         }else{
+    //            // result = middle;
+    //           // high = middle - 1;
+    //             if( middle == 0 || arr[middle] != arr[middle - 1] ){
+    //                 return middle;
+    //             }else{
+    //                 high = middle - 1;
+    //             }
+    //         }
+    //     }
+    //      //return result;
+    //     return -1;
+    // }
+
+    //For Unsorted Arrays
     static int firstOccurence( int[] arr, int num ){
-        int low = 0;
-        int high = arr.length - 1;
-        while( low <= high ){
-            int middle = ( low + high )/2;
-            if( num < arr[middle] ){
-                high = middle - 1;
-            }else if( num > arr[middle] ){
-                low = middle + 1;
-            }else{
-                if( middle == 0 || arr[middle] != arr[middle - 1] ){
-                    return middle;
-                }else{
-                    high = middle - 1;
-                }
+        int result = -1;
+        for(int i = 0; i < arr.length; i++){
+            if( num != arr[i] ){
+                continue;
             }
+            return i;
         }
-        return -1;
+        return result;
     }
 public static void main(String[] args) {
     
