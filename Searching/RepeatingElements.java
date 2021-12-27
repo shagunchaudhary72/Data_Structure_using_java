e1786b8c903d74881479b7906b793db5137bca42
47259_Shagun <chshagun72@gmail.com>
2021-12-27 21:01:06 +0530

    sorting

--- Searching/RepeatingElements.java
+++ Searching/RepeatingElements.java
@@ -4,6 +4,9 @@ public class RepeatingElements {
     //Contitions given :- 
     // 1). only one element can repeat
     // 2). all elements int the range should present =>  0 <= elements range <= Arraylength - 2
-    // 3). Time Complexity = O(n) , AUxilairy Space = O(1)
-    // 4). NO modification allowed in the given arrya
+    // 3). Time Complexity = O(n) , AUxilairy Space = O(n)
+    // 4). NO modification allowed in the given array
+    static int repeatedElement( int[] arr ){
+        
+    }
 }
---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+class BubbleSort{
+
+    static void sort(int[] arr){
+        for(int i = 0; i < arr.length-1; ++i ){
+            for(int j = 0; j < arr.length-i; ++j ){
+                if( arr[j] > arr[j+1] )
+                    swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+    static void swap(int x, int y){
+        int temp = x;
+        x = y;
+        y = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 10, 20, 5, 3, 35, 25 };
+        System.out.println("Initial : " + Arrays.toString(arr));
+        Arrays.sort(arr);
+        System.out.println("Sorted : " + Arrays.toString(arr) );
+    }
+}
