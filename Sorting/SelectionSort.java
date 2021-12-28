67cc4868d9e58844623473b6187a44a0045573db
47259_Shagun <chshagun72@gmail.com>
2021-12-28 22:02:07 +0530

    selection sort

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -3,7 +3,15 @@ import java.util.Arrays;
 public class SelectionSort {
     
     static void selectionSort(int[] arr){
-
+        for(int i = 0; i < arr.length-1; ++i ){
+            for( int j = i+1; j < arr.length; ++j ){
+                if( arr[ i ] > arr[ j ] ){
+                    int temp =  arr[ i ];
+                    arr[ i ] = arr[ j ];
+                    arr[ j ]= temp;
+                }
+            }
+        }
     }
 
     
