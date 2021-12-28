925f393373e9c0b40719217a9f92df63c5d9a751
47259_Shagun <chshagun72@gmail.com>
2021-12-28 21:57:15 +0530

    bubble sort

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -2,24 +2,26 @@ import java.util.Arrays;
 
 class BubbleSort{
 
-    static void sort(int[] arr){
+    static void bubbleSort(int[] arr){
         for(int i = 0; i < arr.length-1; ++i ){
             for(int j = 0; j < arr.length-i-1; ++j ){
                 if( arr[j] > arr[j+1] )
-                    swap(arr[j], arr[j+1]);
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
             }
         }
-    }
-    static void swap(int x, int y){
-        int temp = x;
-        x = y;
-        y = temp;
+        // for (int i : arr) {
+        //     System.out.println(i);
+        // }
     }
 
     public static void main(String[] args) {
         int[] arr = { 10, 20, 5, 3, 35, 25 };
         System.out.println("Initial : " + Arrays.toString(arr));
-        Arrays.sort(arr);
+        bubbleSort(arr);
         System.out.println("Sorted : " + Arrays.toString(arr) );
     }
 }
---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    
+    static void selectionSort(int[] arr){
+
+    }
+
+    
+    public static void main(String[] args) {
+        int[] arr = { 10, 20, 5, 3, 35, 25 };
+        System.out.println("Initial : " + Arrays.toString(arr));
+        selectionSort(arr);
+        System.out.println("Sorted : " + Arrays.toString(arr) );
+    }
+}
