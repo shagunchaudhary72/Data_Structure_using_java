842801672e049d18ee23889550d23c1f091eda14
47259_Shagun <chshagun72@gmail.com>
2021-12-28 21:49:01 +0530

    bubble sort

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -4,7 +4,7 @@ class BubbleSort{
 
     static void sort(int[] arr){
         for(int i = 0; i < arr.length-1; ++i ){
-            for(int j = 0; j < arr.length-i; ++j ){
+            for(int j = 0; j < arr.length-i-1; ++j ){
                 if( arr[j] > arr[j+1] )
                     swap(arr[j], arr[j+1]);
             }
