3458e32bff6bed5ccc6cdfe8c13048b094fd00ab
47259_Shagun <chshagun72@gmail.com>
2022-01-08 16:40:04 +0530

    boundary elements of a matrix

--- matrix/BoundaryElementsOfMatrix.java
+++ matrix/BoundaryElementsOfMatrix.java
@@ -9,14 +9,14 @@ public class BoundaryElementsOfMatrix {
             System.out.println();
         }
     }
-//boundary elements of matrix having equal number of columns in each row
+
     static void boundaryElements(int[][] arr){
         int row = arr.length;
         int column = arr[0].length;
         System.out.print("Boundary Elements :-> ");
         //if matrix have only one row
         if( row == 1){
-            for(int c = 0; c < column; ++c )
+            for(int c = 0; c < arr[0].length; ++c )
                 System.out.print(" " + arr[0][c]);
         }
         //if matrix have only one column per row
@@ -27,13 +27,13 @@ public class BoundaryElementsOfMatrix {
         //if matrix have more than one rows and column
         else{
             //for first row elements
-            for(int c = 0; c < column; ++c )
+            for(int c = 0; c < arr[0].length; ++c )
                 System.out.print(" " + arr[0][c]);
             //for last column elements of middle rows 
             for(int r = 1; r < row-1; ++r )
-                System.out.print(" " + arr[r][column-1]);
+                System.out.print(" " + arr[r][arr[r].length-1]);
             //for last row elements
-            for(int c = column-1; c >= 0; --c )
+            for(int c = arr[row-1].length-1; c >= 0; --c )
                 System.out.print(" " + arr[row-1][c] );
             //for first column elements of middle rows
             for(int r = row-2; r > 0; --r )
@@ -42,10 +42,11 @@ public class BoundaryElementsOfMatrix {
     }
 
     public static void main(String[] args) {
-        int[][] matrix = {{1, 2, 3, 4},{5, 6, 7 , 8},{9, 10, 11, 12}}; //output should be:- 1 2 3 4 8 12 11 10 9 5
+       // int[][] matrix = {{1, 2, 3, 4},{5, 6, 7 , 8},{9, 10, 11, 12}}; //output should be:- 1 2 3 4 8 12 11 10 9 5
         //int[][] matrix = {{1, 2, 3, 4}};//output :- 1 2 3 4
         //int[][] matrix = { {1},{2}, {3}, {4} };//output :- 1 2 3 4
        // int[][] matrix = {{1, 2, 3, 4},{5, 6, 7 , 8}};//output :- 1 2 3 4 8 7 6 5
+       int[][] matrix = {{1, 2, 3, 4},{5, 6 },{9, 10, 11}};//output :- 1 2 3 4 6 11 10 9 5
         originalMatrix(matrix);
         boundaryElements(matrix);
     }
